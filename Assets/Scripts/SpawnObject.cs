cea4c10809c3889e9e5be65ce42f952a9dfa797e
Pati <K00280813@student.tus.ie>
2025-03-20 13:47:49 +0000

    UI items and materials

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class SpawnObject : MonoBehaviour
 {
-    public GameObject cubePrefab;
+    public GameObject malePrefab;
+    public GameObject femalePrefab;
+    public GameObject enemyPrefab;
+    public GameObject treePrefab;
+    public GameObject rockPrefab;
     public GameObject spawnArea;
     private BoxCollider spawnAreaCollider;
+
+    private GameObject currentSpawnedObject;
+
+   
     void Start()
     {
-        spawnAreaCollider = GetComponent<BoxCollider>();
+   spawnAreaCollider = spawnArea.GetComponent<BoxCollider>();
 
     }
 
-
-    public void SpawnCube()
+    private void SpawnObjectAtPosition(GameObject prefab)
     {
+
+        if (currentSpawnedObject != null)
+        {
+            // If the object is inside the spawn area, destroy it
+            if (IsObjectInSpawnArea(currentSpawnedObject))
+            {
+                Destroy(currentSpawnedObject);
+            }
+        }
+
+
         Vector3 spawnCenter = spawnAreaCollider.transform.position + spawnAreaCollider.center;
         Vector3 spawnSize = spawnAreaCollider.size;
         float xPos = Random.Range(spawnCenter.x - spawnSize.x / 2, spawnCenter.x + spawnSize.x / 2);
@@ -23,7 +41,61 @@ public class SpawnObject : MonoBehaviour
         float zPos = Random.Range(spawnCenter.z - spawnSize.z / 2, spawnCenter.z + spawnSize.z / 2);
 
         Vector3 spawnPosition = new Vector3(xPos, yPos, zPos);
-        Instantiate(cubePrefab, spawnPosition, Quaternion.identity);
 
+
+        currentSpawnedObject = Instantiate(prefab, spawnPosition, Quaternion.identity);
+    }
+
+    private void Update()
+    {
+
+        // Check if the object is still in the spawn area, and destroy it if necessary
+        if (currentSpawnedObject != null && IsObjectInSpawnArea(currentSpawnedObject))
+        {
+            // If the current object is inside the spawn area, we don't need to destroy it yet
+            return;
+        }
+
+        // If the object is outside the spawn area, allow for the next object to be spawned
+        if (currentSpawnedObject != null && !IsObjectInSpawnArea(currentSpawnedObject))
+        {
+            currentSpawnedObject = null; // Object is outside the spawn area, so reset
+        }
+    }
+
+    private bool IsObjectInSpawnArea(GameObject obj)
+    {
+        Collider objectCollider = obj.GetComponent<Collider>();
+        if (objectCollider != null && spawnAreaCollider.bounds.Intersects(objectCollider.bounds))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    public void SpawnMale()
+    {
+        SpawnObjectAtPosition(malePrefab);
+    }
+
+    public void SpawnFemale()
+    {
+        SpawnObjectAtPosition(femalePrefab);
+    }
+
+    public void SpawnEnemy()
+    {
+        SpawnObjectAtPosition(enemyPrefab);
+    }
+
+    public void SpawnTree()
+    {
+        SpawnObjectAtPosition(treePrefab);
+    }
+
+    public void SpawnRock()
+    {
+        SpawnObjectAtPosition(rockPrefab);
     }
 }
