e77e29340cd08b59da8c6aa8b6a82efe7d1653be
Pati <K00280813@student.tus.ie>
2025-04-29 16:13:35 +0100

    Working on dice number display

---
+++ Assets/Scripts/DiceNumberDisplay.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DiceNumberDisplay : MonoBehaviour
+{
+    public TextMeshPro textMesh;
+    [HideInInspector] public int maxRoll = 20; // Set from spawner
+
+    private void Start()
+    {
+        int result = Random.Range(1, maxRoll + 1);
+        textMesh.text = result.ToString();
+        Destroy(gameObject, 10f);
+    }
+}
